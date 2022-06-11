7fbd9c75da43085682220195563d836ed2d0c193
ln-dev7 <leonelngoya@gmail.com>
2022-06-11 04:45:10 +0100

    Hello world

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Hello City</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Overpass:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            *, *:before, *:after {
+                box-sizing: border-box;
+                padding: 0;
+                margin: 0;
+                scroll-behavior: smooth;
+                font-family: 'Overpass', sans-serif;
+            }
+            html, body {
+                height: 100%;
+            }
+            body{
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+        </style>
+    </head>
+    <body class="antialiased">
+        <h1>Hello from Camerun</h1>
+    </body>
+</html>
